d8bec361206402e308e49ff61d89d12ad8ef2411
shiva <shankuu786@gmail.com>
2024-02-29 06:17:23 +0000

    first

---
+++ src/components/LoginForm/index.js
@@ -0,0 +1,113 @@
+import {Redirect} from 'react-router-dom'
+import {Component} from 'react'
+
+import Cookies from 'js-cookie'
+
+import './index.css'
+
+class LoginForm extends Component {
+  state = {userName: '', password: '', showSubmitError: false, errorMsg: ''}
+
+  onChangeUsersname = event => {
+    this.setState({userName: event.target.value})
+  }
+
+  onChangePassword = event => {
+    this.setState({password: event.target.value})
+  }
+
+  renderPasswordField = () => {
+    const {password} = this.state
+    return (
+      <>
+        <label className="input-label" htmlFor="password">
+          PASSWORD
+        </label>
+        <input
+          type="password"
+          id="password"
+          className="input-field"
+          value={password}
+          onChange={this.onChangePassword}
+        />
+      </>
+    )
+  }
+
+  renderUsernameField = () => {
+    const {username} = this.state
+    return (
+      <>
+        <label className="input-label" htmlFor="username">
+          USERNAME
+        </label>
+        <input
+          type="text"
+          id="username"
+          className="input-field"
+          value={username}
+          onChange={this.onChangeUsername}
+        />
+      </>
+    )
+  }
+
+  onSubmitSuccess = jwtToken => {
+    Cookies.set(jwtToken, 'jwt_token', {expires: 30})
+    const {history} = this.props
+    history.replace('/')
+  }
+
+  onSubmitFailure = errorMsg => {
+    console.log(errorMsg)
+    this.setState({showSubmitError: true, errorMsg})
+  }
+
+  submitForm = async event => {
+    event.preventDefault()
+    const {userName, password} = this.state
+    const userDetails = {userName, password}
+    const url = 'https://apis.ccbp.in/login'
+    const options = {
+      method: 'POST',
+      body: JSON.stringify(userDetails),
+    }
+    const response = await fetch(url, options)
+    const data = await response.json()
+    if (response.ok === true) {
+      this.onSubmitSuccess(data.jwt_token)
+    } else {
+      this.onSubmitFailure(data.error_msg)
+    }
+  }
+
+  render() {
+    const {errorMsg, showSubmitError} = this.state
+
+    const jwtToken = Cookies.get('jwt_token')
+    if (jwtToken !== undefined) {
+      ;<Redirect to="/" />
+    }
+    return (
+      <div className="bg-container">
+        <div className="form-container">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/logo-img.png"
+            alt="website logo"
+            className="website-logo"
+          />
+          <form onSubmit={this.submitForm}>
+            <div className="input-container">{this.renderUsernameField()}</div>
+            <div className="input-container">{this.renderPasswordField()}</div>
+            <button type="submit" className="login-button">
+              Login
+            </button>
+            {showSubmitError && <p className="error-message">*{errorMsg}</p>}
+          </form>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default LoginForm
